3addcba95eb4be7b1331977fba2c006fc8c4bf4c
Haythem-Jaidane <haythemjaidane11@gmail.com>
2022-09-27 17:55:24 -0500

    Connect our wallet to our web app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
@@ -7,6 +7,57 @@ export default function App() {
   const wave = () => {
     
   }
+
+  const [currentAccount,setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async() => {
+      try{
+          const { ethereum } = window;
+          if(!ethereum){
+              console.log("Make sure you have metamask!");
+          }
+          else{
+	      console.log("We have the ethereum object",ethereum);
+          }
+
+          const accounts = await ethereum.request({ method:"eth_accounts"});
+
+          if(accounts.lendth !== 0){
+              const account = accounts[0];
+	      console.log("Found an authorized account:",account);
+	      setCurrentAccount(account)
+          }
+          else{
+              console.log("No authorized account found")
+          }
+      }
+      catch(error){
+          console.log(error);
+      }
+  }
+
+  const connectWallet = async() => {
+      try{
+          const {ethereum} = window;
+
+	  if(!ethereum){
+	      alert("Get MetaMask!");
+	      return;
+          }
+
+	  const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+	      
+	  console.log("Connected", accounts[0]);
+	  setCurrentAccount(accounts[0]);
+      }
+      catch(error){
+          console.log(error)
+      }
+   }
+
+  useEffect(() => {
+      checkIfWalletIsConnected();
+  },[])
   
   return (
     <div className="mainContainer">
@@ -24,6 +75,12 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Add Me
         </button>
+
+	{!currentAccount && (
+	    <button className="waveButton" onClick={connectWallet}>
+	        Connect Wallet
+	    </button>
+        )}
       </div>
     </div>
   );
