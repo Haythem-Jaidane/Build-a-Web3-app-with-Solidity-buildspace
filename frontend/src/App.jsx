e42938a52e27c95e7c8478b0fdcbc45ee25bc528
Haythem-Jaidane <haythemjaidane11@gmail.com>
2022-09-27 19:15:42 -0500

    Call the deployed smart contract from our web app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from "./WavePortal.json";
 
 export default function App() {
+  
+  const contractAddress = "0x9D9f79d14F8f4Fd6f70A11Ab392629F35FD17731"
 
-  const wave = () => {
-    
+  const contractABI = abi.abi;
+  
+  const wave = async () => {
+    try{
+        const {ethereum} = window;
+
+	if(ethereum){
+	    const provider = new ethers.providers.Web3Provider(ethereum);
+	    const signer = provider.getSigner();
+	    const wavePortalContract = new ethers.Contract(contractAddress, contractABI,signer);
+
+	    let count = await wavePortalContract.getTotalWaves();
+	    console.log("Retrieved total wave count ...",count.toNumber());
+
+            const waveTxn = await wavePortalContract.addWave();
+	    console.log("Mining...", waveTxn.hash);
+
+	    await waveTxn.wait();
+	    console.log("Mined -- ", waveTxn.hash);
+
+	    count = await wavePortalContract.getTotalWaves();
+	    console.log("Retrieved total wave count...",count.toNumber());
+	}
+	else{
+	    console.log("Ethereum object doesn't exist!");
+	}
+     }
+     catch(error){
+         console.log(error);
+     }
+
   }
 
   const [currentAccount,setCurrentAccount] = useState("");
